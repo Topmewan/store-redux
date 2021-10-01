d4185ba728ff2e3bbaa48600ca5f923a6d71a680
Topmewan <anon8733@gmail.com>
2021-10-02 00:53:24 +0300

    render correct produtcs

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -3,19 +3,28 @@ import {useSelector} from "react-redux";
 
 const ProductComponent = () => {
     const products = useSelector((state)=> state.allProducts.products);
-    const {id, title} = products[0];
-
-    return(
-        <div className="four column wide">
+    const renderList = products.map((products) => {
+        const {id,title,image,price,category} = products;
+        return (<div className="four wide column" key={id}>
             <div className="ui link cards">
                 <div className="card">
-                    <div className="image"></div>
+                    <div className="image">
+                        <img src={image} alt={title}/>
+                        </div>
                     <div className="content">
                         <div className="header">{title}</div>
+                        <div className="meta price">$ {price}</div>
+                        <div className="meta">{category}</div>
                     </div>
                 </div>
             </div>
-        </div>
+        </div>);
+    })
+
+    return (
+        <>
+            {renderList}</>
     );
+
 };
 export default ProductComponent;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,17 +1,20 @@
 import React, {useEffect} from "react";
 import axios from "axios";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import ProductComponent from "./ProductComponent";
+import {setProducts} from "../redux/actions/productActions";
 
 
 const ProductList = () => {
     const products = useSelector((state)=> state);
+    const dispatch = useDispatch();
 
     const fetchProducts = async () => {
         const response = await axios.get("https://fakestoreapi.com/products").catch((err) => {
             console.log("Err",err)
         });
-        console.log(response)
+        dispatch(setProducts(response.data));
+
     };
 
     useEffect(() => {
