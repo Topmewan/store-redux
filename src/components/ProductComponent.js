3f5f5a3707d6e9c82731ab24e42e80b75cae13b7
Topmewan <anon8733@gmail.com>
2021-10-02 01:10:49 +0300

    add styles for images

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -5,20 +5,22 @@ const ProductComponent = () => {
     const products = useSelector((state)=> state.allProducts.products);
     const renderList = products.map((products) => {
         const {id,title,image,price,category} = products;
-        return (<div className="four wide column" key={id}>
-            <div className="ui link cards">
-                <div className="card">
-                    <div className="image">
-                        <img src={image} alt={title}/>
+        return (
+            <div className="four wide column" key={id}>
+                <div className="ui link cards">
+                    <div className="card">
+                        <div className="image">
+                            <img src={image} alt={title}/>
                         </div>
-                    <div className="content">
-                        <div className="header">{title}</div>
-                        <div className="meta price">$ {price}</div>
-                        <div className="meta">{category}</div>
+                        <div className="content">
+                            <div className="header">{title}</div>
+                            <div className="meta price">$ {price}</div>
+                            <div className="meta">{category}</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>);
+        );
     })
 
     return (
