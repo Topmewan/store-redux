225b603048f95eed664befdc01767f8fb54fb0b7
Topmewan <anon8733@gmail.com>
2021-10-02 00:14:27 +0300

    change bootstrap to semantic-ui, i want to ty it

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = (props) => {
+    return(
+        <div className="ui fied menu">
+            <div className="ui container center">
+                <h2>My Shop</h2>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Header;
