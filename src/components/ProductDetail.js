0b0133e6a69ce42c6cf59aa175c6fc743862f25c
Topmewan <anon8733@gmail.com>
2021-10-02 14:34:10 +0300

    get first product test

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import {useDispatch, useSelector} from "react-redux";
+import {selectedProducts} from "../redux/actions/productActions";
 
 const ProductDetails = () => {
+    const product = useSelector((state) => state.product);
+    const {productId} = useParams();
+    const dispatch = useDispatch();
+    console.log(product);
+
+    const fetchProductDetail = async () => {
+        const response = await axios
+            .get(`https://fakestoreapi.com/products/${productId}`)
+            .catch(err => console.log('err',err));
+
+        dispatch(selectedProducts(response.data));
+    };
+
+    useEffect(()=> {
+        if(productId && productId !== "") fetchProductDetail();
+    },[productId]);
+
     return(
         <div>
-            <h1>Product</h1>
+
         </div>
     );
-}
+};
+
 export default ProductDetails;
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -13,3 +13,13 @@ export const productReducer = (state= initialState, {type,payload}) => {
 
     }
 };
+
+export const selectedProductReducer = (state={}, {type,payload}) => {
+    switch (type) {
+        case ActionTypes.SELECTED_PRODUCTS:
+            return {...state, ...payload}
+        default:
+            return state;
+    }
+
+};
